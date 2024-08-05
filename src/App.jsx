3de4fdc73a5e3bb0534dffa19a977d2243c5ffb5
sofi1205 <mendezsofia735@gmail.com>
2024-08-05 11:39:43 -0300

    conectei a lista no main

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
+import {useState} from 'react'
+import Listagem from './pages/Listagem'
 import { Routes,Route } from 'react-router-dom'
 import './App.css'
 
 function App() {
   const App = () => {
+
+    const [color] = useState("#0000ff")
+    document.body.style.backgroundColor = color
     return (
       <Routes>
-        <Route path="/" />
+        <Route path="/" element={<Listagem />} />
       </Routes>
     )
   }
---
+++ src/pages/Listagem.jsx
@@ -0,0 +1,12 @@
+import React, {useState, useEffect} from 'react'
+
+const Listagem = () => {
+    return (
+        <>
+        <h2>Pizzas </h2>
+        </>
+    )
+}
+
+export default Listagem
+
