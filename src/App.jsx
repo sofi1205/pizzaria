1bd76f5d2523d134fac2edfdc9b9451ec01d30ce
sofi1205 <mendezsofia735@gmail.com>
2024-08-06 08:38:35 -0300

    arrumei o erro q tinha no app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,18 @@ import Listagem from './pages/Listagem';
 
 
 
-function App() {
-  const App = () => {
+const App = () => {
 
     const [color] = useState("#0000ff")
     document.body.style.backgroundColor = color
+    
     return (
       <Routes>
         <Route path="/" element={<Listagem />} />
       </Routes>
     )
   }
-}
+
 
 export default App;
 
