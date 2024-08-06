86c043db52c623a5307536508e5f298e5a9bc434
sofi1205 <mendezsofia735@gmail.com>
2024-08-06 09:17:06 -0300

    verificar se listagem e pizza.jsx está correto?

--- src/pages/Listagem.jsx
+++ src/pages/Listagem.jsx
@@ -1,11 +1,33 @@
 import React, {useState, useEffect} from 'react'
 
 const Listagem = () => {
+
+    const ItensLista = () => Pizzas.map(
+        pizza =>
+        <li key={pizza.id} style={{listStyle: 'none'}}>
+            <img src={pizza.imagem}
+            alt={pizza.nome}
+            style={{
+                width:"150",
+                height:"100px",
+                borderRadius:"8px",
+                margin: "0px",
+            }}
+            />
+
+        </li>
+    )
+
     return (
         <>
-        <h2>Pizzas </h2>
+        <h2 style = {{textAlign: 'center'}}>Pizzas ITB </h2>
+        <ul> <ItensLista /> 
+        </ul>
         </>
     )
-}
+
+    }
+
+
 
 export default Listagem
